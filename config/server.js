ed22b4bd37d19a6dab2fb8c355ae817e3ad9d472
Isaac Farciert <rfisaac2296@yahoo.com>
2020-09-07 17:32:10 -0400

    initial commit w preliminary models

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f459c712516165ff895572b5e66a9ff0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f4347794-5613-410c-9fd1-c6c42a9fa471'
+};
